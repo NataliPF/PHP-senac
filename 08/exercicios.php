467b2f7f314d0194ec2d61ea3f0f70ed2b692846
Nátali <nana_projetti@hotmail.com>
2024-03-28 19:51:51 -0300

    Aula Finalizada BREAK CONTINUE

---
+++ 08/exercicios.php
@@ -0,0 +1,53 @@
+<?php
+//// DESAFIOS LÓGICOS - ESTRUTURAS DE REPETIÇÃO
+
+
+
+
+// Desafio 1: Contagem Regressiva
+/*
+    Crie um programa que solicite ao usuário um número inteiro positivo N e realize uma contagem regressiva até zero.
+    Após cada contagem, exiba a mensagem "Falta X segundos" onde X representa o número de segundos restantes para chegar a zero.
+*/
+
+
+// Solução com for
+
+
+// Solução com while
+
+
+// Solução com do-while
+
+
+// Desafio 2: Tabuada
+/*
+    Crie um programa que exiba a tabuada de multiplicação de um número fornecido pelo usuário.
+    A tabuada deve ser exibida de 1 até 10.
+*/
+
+
+// Solução com while
+
+
+// Solução com do-while
+
+
+
+// Desafio 3: Sequência Fibonacci
+/*
+    A sequência de Fibonacci é uma sequência de números inteiros em que cada termo é a soma dos dois anteriores.
+    Crie um programa que solicite ao usuário um número N e exiba os N primeiros termos da sequência de Fibonacci.
+*/
+
+
+
+// Solução com for
+
+
+// Solução com while
+
+
+// Solução com do-while
+
+?>
