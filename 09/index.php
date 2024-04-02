13fc063f47fd00c9d37776d9332c5b70e7441b41
Nátali <nana_projetti@hotmail.com>
2024-04-02 20:43:59 -0300

    09 - Funções

--- 09/index.php
+++ 09/index.php
@@ -13,7 +13,7 @@ saudacao(); // Chamada da função
 
 // Declaração de uma função que retorna um valor 
 function saudacao_retorno() {
-    return "Olá, TII04 <br>";
+    return "Olá, TII04! <br>";
 }
 
 echo saudacao_retorno(); // Chamada de função com retorno
@@ -23,5 +23,30 @@ function saudacao_parametro($nome) {
     echo "Olá, $nome& <br>";
 }
 
-saudacao_parametro("TII04");
+saudacao_parametro("TII04"); // Chamada de função com retorno
+
+// Declaração de uma função que recebe um parâmetro e retorno algo
+function saudacao_parametro_retorno($nome) {
+    return "Olá, $nome* <br> ";
+}
+
+echo saudacao_parametro_retorno( "TII04" ); 
+function soma ($a, $b) {
+    return $a + $b;
+    }
+
+    $resultado = soma (8, 16);
+    echo "A soma de 8 com 16 é:" . soma(8, 16) . "<br>";
+
+    # ESCOPO DAS VARIÁVEIS!!!
+
+    $mensagem = "Olá"; // Variável global
+
+    function mostrarMensagem() {
+        $mensagem = "Mundo"; // Variável local
+        echo $mensagem; // Saída: Mundo
+    }
+
+    mostrarMensagem();
+    echo $mensagem; // Saída: Olá
 ?>
