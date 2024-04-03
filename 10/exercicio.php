821c61f2740fa02a78a843e7a8417fbd494bb388
Nátali <nana_projetti@hotmail.com>
2024-04-02 21:51:48 -0300

    Exercícios

---
+++ 10/exercicio.php
@@ -0,0 +1,24 @@
+<?php
+/*
+1 - Defina uma classe chamada Veiculo com as seguintes propriedades:
+- marca
+- modelo
+- ano
+- ligado (indicando se o veículo está ligado ou desligado)
+
+2 - Implemente um método construtor na classe Veiculo que inicialize as propriedades marca, modelo e ano. O método construtor também deve definir a propriedade "ligado" como false por padrão.
+3 - Implementar métodos na classe Veículo:
+- ligar(): Este método deve alterar a propriedade "ligado" pra true, se o veículo estiver desligado.
+- desligar(): Este método deve alterar a propriedade "ligado" para false, se o veículo estiver ligado.
+- exibirDados(): Este método deve exibir as informações do veículo(marca, modelo, ano, e status de ligado ou desligado)
+*/
+// 1 exercicio
+ class  Veiculos{
+    public $marca;
+    public $modelo;
+    public $ano;
+ }
+
+
+
+?>
