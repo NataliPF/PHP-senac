20b786b0ef89cbb0952c8ff41085a39ef0b46771
Nátali <nana_projetti@hotmail.com>
2024-04-03 19:25:42 -0300

    Finalização dos Exercícios

--- 10/exercicio.php
+++ 10/exercicio.php
@@ -12,13 +12,47 @@
 - desligar(): Este método deve alterar a propriedade "ligado" para false, se o veículo estiver ligado.
 - exibirDados(): Este método deve exibir as informações do veículo(marca, modelo, ano, e status de ligado ou desligado)
 */
-// 1 exercicio
- class  Veiculos{
+class Veiculo {
     public $marca;
     public $modelo;
     public $ano;
- }
+    public $ligado;
 
+    public function __construct($marca, $modelo, $ano)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->ano = $ano;
+        $this->ligado = false;
+    }
 
+    public function ligar() {
+        $this->ligado == false ? $this->ligado = true : null;
+    }
+
+    public function desligar() {
+        $this->ligado == true ? $this->ligado = false : null;
+    }
+
+    public function exibirDados() {
+        echo "Marca: $this->marca, Modelo: $this->modelo, Ano: $this->ano, Ligado: $this->ligado <br>";
+    }
+}
+$meuCarro = new Veiculo("Toyota", "Corolla", 2022);
+
+// Exibindo dados do veículo
+$meuCarro->exibirDados(); // Marca: Toyota, Modelo: Corolla, Ano: 2022, Ligado: false
+
+// Ligando o veículo
+$meuCarro->ligar();
+
+// Exibindo dados atualizados
+$meuCarro->exibirDados(); // Marca: Toyota, Modelo: Corolla, Ano: 2022, Ligado: true
+
+// Desliga o veículo
+$meuCarro->desligar();
+
+// Exibindo dados atualizados
+$meuCarro->exibirDados(); // Marca: Toyota, Modelo: Corolla, Ano: 2022, Ligado: false
 
 ?>
