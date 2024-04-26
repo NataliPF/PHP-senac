a864e5154c735f779d5390db42ef3bbbb4d68cda
Nátali <nana_projetti@hotmail.com>
2024-04-25 21:49:17 -0300

    19- Controller Handler

--- 19/index.php
+++ 19/index.php
@@ -1,17 +1,17 @@
 <?php
-require_once 'database/PedidoRepository.php';
-require_once 'database/ProdutoRepository.php';
+require_once 'controller/PedidoController.php';
 
+$entity = $_GET['entity'];
 $action = $_GET['action'];
 
-switch($action) {
-    case 'listar':
-        echo json_encode(PedidoRepository::getAllPedidos());
+switch($entity) {
+    case 'pedido':
+        require_once 'controller/PedidoController.php';
+        PedidoController::handleRequest($action);
         break;
-        case 'listarProdutos':
-            echo json_encode(ProdutoRepository::getAllProdutos());
-            break; 
-        default:
+    default:
+        http_response_code(400);
+        echo json_encode(['error' => 'Entidade inválida!']);
         break;
 }
 
